6849ce9b1abe96da512edc755a59ad516fbc1c67
Nina <59759345+Shamo-Nina@users.noreply.github.com>
2020-01-11 16:25:33 +0800

    動態產生第3顆button

---
+++ 動態產生第3顆button/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Button a = new Button();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Point p = new Point(126, 188);
+            a.Location = p;
+            Size s = new Size(82, 46);
+            a.Size = s;
+            a.Text = "button3";
+
+            //a.Visible = true;
+            a.Enabled = false;
+            this.Controls.Add(a);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a.Enabled = true;
+        }
+    }
+}
