d1865d58c46fcc596946372a1f8f337649d99a18
Nina <59759345+Shamo-Nina@users.noreply.github.com>
2020-01-11 16:24:35 +0800

    動態產生TexBox

---
+++ 動態產生TexBox/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        Button a = new Button();
+        TextBox t = new TextBox();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Point p = new Point(117, 114);
+            a.Location = p;
+            Size s = new Size(100, 52);
+            a.Size = s;
+            a.Text = "button2";
+            Point p1 = new Point(117, 184);
+            t.Location = p1;
+            Size s1 = new Size(100, 22);
+            t.Size = s1;
+
+            //a.Visible = true;
+            a.Enabled = true;
+            t.Enabled = false;
+            this.Controls.Add(a);
+            this.Controls.Add(t);
+
+            a.Click += A_Click;
+
+        }
+
+        private void A_Click(object sender, EventArgs e)
+        {
+            t.Enabled = true;
+            t.Text = "input";
+            //throw new NotImplementedException();
+        }
+    }
+}
